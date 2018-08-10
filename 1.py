b77d432f0c46b478a3ea95e950055575d9dd84df
twinsavita <twinsavita@gmail.com>
2018-08-10 19:39:50 +0000

    CHeck

--- 1.py
+++ 1.py
@@ -1,10 +1,13 @@
 import argparse
-parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--firmware_list", nargs="*", required=True, help="The older firmware files",default=[])
 args = parser.parse_args()
 
 print "111111111111111111111111111111111111111111111"
 print args.firmware_list
+print len(args.firmware_list)
+for i in len(args.firmware_list):
+    print args.firmware_list[i]
 print "222222222222222222222222222222222222222222222"
 
 hi = "Savitha"
