821bb1641a0dd6247fe084d14841457a3a28d130
twinsavita <twinsavita@gmail.com>
2018-08-10 19:42:17 +0000

    CHeck

--- 1.py
+++ 1.py
@@ -6,7 +6,7 @@ args = parser.parse_args()
 print "111111111111111111111111111111111111111111111"
 print args.firmware_list
 print len(args.firmware_list)
-for i in len(args.firmware_list):
+for i in range(len(args.firmware_list)):
     print args.firmware_list[i]
 print "222222222222222222222222222222222222222222222"
 
