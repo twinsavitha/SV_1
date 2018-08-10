346fa04ad644ff85e12e04810c6446f45b5501a3
twinsavita <twinsavita@gmail.com>
2018-08-10 19:36:48 +0000

    CHeck

--- 1.py
+++ 1.py
@@ -1,2 +1,9 @@
+import argparse
+parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
+parser.add_argument("-f", "--firmware_list", nargs="*", required=True, help="The older firmware files",default=[])
+args = parser.parse_args()
+
+print args.firmware_list
+
 hi = "Savitha"
 print hi
